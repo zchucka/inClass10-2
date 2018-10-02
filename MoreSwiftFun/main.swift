b8390a896234de87d97dd3f3d2cde648dbb76c1b
Chucka, Zachary Tyler <zchucka@jbaa-23732l.gonzaga.edu>
2018-10-02 09:32:19 -0700

    Initial Commit

---
+++ MoreSwiftFun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MoreSwiftFun
+//
+//  Created by Chucka, Zachary Tyler on 10/2/18.
+//  Copyright © 2018 Chucka, Zachary Tyler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
